921638e4242d102c55516940461e6b6c0cc5fcde
tedeaston1 <edeaston@outlook.com>
2023-11-30 11:12:59 +0000

    initial commit

---
+++ api/src/main/java/com/tedapplication/basicfeaturesbackend/BasicFeaturesBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tedapplication.basicfeaturesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicFeaturesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicFeaturesBackEndApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/tedapplication/basicfeaturesbackend/BasicFeaturesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedapplication.basicfeaturesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFeaturesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
